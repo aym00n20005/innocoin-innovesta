9715975c46c5da3299c54b8598382a67527b7efe
aym00n20005 <149860867+aym00n20005@users.noreply.github.com>
2025-03-08 12:52:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCpqIk8H4a6rcm-EMTkbJ9vBCc-ZWZznGU",
+  authDomain: "innocoin-fe53a.firebaseapp.com",
+  projectId: "innocoin-fe53a",
+  storageBucket: "innocoin-fe53a.firebasestorage.app",
+  messagingSenderId: "151162681567",
+  appId: "1:151162681567:web:b5d255690e9fcf9012916a"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-
-}
-
-loadLeaderboard();
